32c9d696ea4f9f0b8288777105099dcfe1e6656d
szabolaszlogit <szabolaszlo.mail@gmail.com>
2022-04-20 17:37:32 +0200

    telszam /

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,22 @@
 <template>
-<b-container fluid class=" bg-dark">
-     <b-container class="bv-example-row">
-    <b-row>
-      <b-col>
-        <b-card
-          class="mb-2  bg-dark text-light "
-
-        >
-          <div>
-        <p>Maróthy János Általános Iskola</p>
-        <p>4971 Rozsály, Kossuth utca 37/a.</p>
-        <p>+36 44/368-909</p>
-        <p>OM azonosító: 033463</p>
-          </div>
-
-          </b-card-text>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container> 
-</b-container>
-
+  <b-container fluid class="bg-dark">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <b-card class="mb-2 bg-dark text-light">
+            <b-card-text>
+              <div>
+                <p>Maróthy János Általános Iskola</p>
+                <p>4971 Rozsály, Kossuth utca 37/a.</p>
+                <p>+36 44/368-909</p>
+                <p>OM azonosító: 033463</p>
+              </div>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-container>
 </template>
 
-<script>
-</script>
+<script></script>
